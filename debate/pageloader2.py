458d846a0a71f6b4f12cc9f83aa041fbabadfc88
gordodev <carlylespeaks@gmail.com>
2020-09-16 22:41:15 -0400

    2nd commit

--- debate/pageloader2.py
+++ debate/pageloader2.py
@@ -1,19 +1,48 @@
 import webbrowser
 import os
 import time
+import subprocess
+import re
+
+#import urllib.request.urlopen
+
+from urllib.request import urlopen
 
 sitesFile = []
-#sites = []
 
+#Read the websites file
 with open('sites.txt') as f:
     sites = f.readlines()
 
+#Format sites and insert into list
 sites = [x.strip() for x in sites]
 
 
-print ('Checking the following channels: \n',sites)
+print ('Checking the following channels: \n',sites,'\n\n\n')
 
-
+'''
+#open sites in the sites list
 for site in sites:
-    webbrowser.open(site)  # Go to google.com
     print ('Site: ',site)
+    webbrowser.open(site)  # Go to google.com
+'''
+
+keywords = ['deputies','police','ambush']
+
+'''
+for site, word in zip(sites, keywords):
+    print (os.system('curl site | grep -oi move'))
+    os.system('curl site | grep -oi move')
+'''
+
+for site, word in zip(sites, keywords):
+    print ('Here is site/word: ',site,word)
+    #subprocess.call(["curl", site, "|", "grep", "-oi", "move"])
+    html_content = urlopen(site).read().decode('utf-8')
+    
+    matches = re.findall('peace', html_content);
+    
+    if len(matches) == 0:
+        print ('I did not find anything')
+    else:
+        print ('My string is in the html')
