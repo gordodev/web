b74cd6d721d4ae0ccd8ec29a8cc5f2fc7226cea6
gordodev <carlylespeaks@gmail.com>
2020-09-22 21:35:22 -0400

    clean up, speed up

--- debate/pageloader.py
+++ debate/pageloader.py
@@ -10,41 +10,32 @@ import time
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import webbrowser
 
 
-# Sample strings.
-list = ["dog dot", "data day", "no match"]
-
+sitesMatched = []
 
 #Read the websites file
 with open('sites.txt') as f:
     sites = f.readlines()
-    #sites[-1] = sites[-1].strip()
-    #sites = f.readlines
 
 
-# Loop.
-#sites = ['google.com','yahoo.com']
-#text = ['riot','violence','judge','leftist','antifa','black bloc','racist','portland']
-#text = ['rochester']
+#SAMPLE KEYWORDS for reference only!!
+#text = ['trump','riots','leftist','anarchist jurisdictions','whistleblower','the trump','the the three','the sjd fef four']
+#text = ['160 million']
 
-text = ['trump','riots','leftist','anarchist jurisdictions','whistleblower','the trump','the the three','the sjd fef four']
+#                          GET KEYWORDS FROM USER
+text = input('Enter keywords: \n').split(" ")
+print (text)
 
-
-#print ('page: ',pagetxt,'\n')
-
+#                          GO TO EACH SITE and parse HTML
 for site in sites:
-    #print ('\n',site)
-
     url = site
     html = urlopen(url).read()
+    html = html.decode('utf-8')
     soup = BeautifulSoup(html, features="html.parser")
     #soup = BeautifulSoup("<a aria-label>",'lxml')
-    '''  
-    # kill all script and style elements
-    for script in soup(["script", "style"]):
-        script.extract()    # rip it out
-    '''
+    
     print(site)    
     for words in text:
 
@@ -59,33 +50,22 @@ for site in sites:
         amount = len(m); #print (amount)
         if m:
             site = site.rstrip("\n")
-            #print (site,'   <========    ',words)
+            #sitesMatched = sitesMatched.insert(0,site)   #Add site to sites matched list to open later
+            sitesMatched.insert(0,site)   #Add site to sites matched list to open later
             print ('             TOTAL: ',words,'=',amount,'\n')
         #time.sleep(0.001)
         #print ('text: ',text)
 
-        '''
-        # break into lines and remove leading and trailing space on each
-        lines = (line.strip() for line in text.splitlines())
-        # break multi-headlines into a line each
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        # drop blank lines
-        text = '\n'.join(chunk for chunk in chunks if chunk)
-        '''
         #print(text)
         time.sleep(0.01)
+#print (sitesMatched)
 
+if len(sitesMatched):
+    for page in sitesMatched:
+        print (sitesMatched)
+        webbrowser.open(page)  # Go to google.com
+        #siteLine = site
+        #print ('Site: ',siteLine)
+else:
+    print ('Not found')
 
-'''
-    for element in text:
-        # Match if 2 words starting with letter "d."
-        print ('\nCheck site for: ',element,'\n')
-        m = re.findall(element, pagetxt)
-
-        # See if success.
-        if m:
-            print(m)
-    time.sleep(1)
-
-#X = ','.join( str(a) for a in [1,2,3] )
-'''
